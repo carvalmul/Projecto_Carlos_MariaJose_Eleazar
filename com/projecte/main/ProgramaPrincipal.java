4cc64c6c59de3efd4a30a4acdcc236dfbb638839
carlos <carlos@correo.com>
2026-04-22 15:20:25 +0200

    primer commit

---
+++ com/projecte/main/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package com.projecte.main;
+
+import com.projecte.carlos.Carlos;
+
+public class ProgramaPrincipal {
+    public static void inici() {
+        Carlos.mostrarNom();
+    }
+
+    public static void main(String[] args) {
+        inici();
+    }
+}
